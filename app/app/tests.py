9f810eb897f971d315dac7de6ba91c773e50b5f2
Ananda Naik <anandanaik@Anandas-MacBook-Pro.local>
2025-10-25 16:15:18 +0530

    Added db configurations

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalsTests(SimpleTestCase):
+    """Test the cal module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers"""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def tests_subtract_numbers(self):
+        """Test subtract numbers"""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, -5)
